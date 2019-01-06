7bd229dc4ff6f829336cda84115787e6bdcefe0a
fhtanaka <fhktanaka@gmail.com>
2019-01-05 22:22:31 -0200

    Added option to generator to create points in the generator class

--- generator.py
+++ generator.py
@@ -3,6 +3,9 @@ from torch import nn, optim
 from torch.autograd.variable import Variable
 from torchvision import transforms, datasets
 
+def noise(quantity, size):
+    return Variable(torch.randn(quantity, size))
+
 class GeneratorNet(torch.nn.Module):
     """
     A three hidden-layer generative neural network
@@ -10,8 +13,9 @@ class GeneratorNet(torch.nn.Module):
     def __init__(self, out_features, leakyRelu=0.2, hidden_layers=[256, 512, 1024], in_features=100, escalonate=False):
         super(GeneratorNet, self).__init__()
         
+        self.in_features = in_features
         self.layers = hidden_layers.copy()
-        self.layers.insert(0, in_features)
+        self.layers.insert(0, self.in_features)
 
         for count in range(0, len(self.layers)-1):
             self.add_module("hidden_" + str(count), 
@@ -34,7 +38,16 @@ class GeneratorNet(torch.nn.Module):
                     nn.Linear(self.layers[-1], out_features)
                 )
             )
+    
     def forward(self, x):
         for name, module in self.named_children():
             x = module(x)
         return x
+
+    def create_data(self, quantity):
+        points = noise(quantity, self.in_features)
+        try:
+            data=self.forward(points.cuda())
+        except RuntimeError:
+            data=self.forward(points.cpu())
+        return data
