03b1a31e6967180c2a9ad48777a72a653548767e
fhtanaka <fhktanaka@gmail.com>
2019-01-10 08:41:17 -0200

    Organized the files a little better

--- compare_data.py
+++ compare_data.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 import glob
 from data_treatment import DataAtts
 from IPython.display import display
+from sklearn.tree import DecisionTreeClassifier as DT
+from sklearn.tree import export_graphviz # Decision tree from sklearn
+import pydotplus # Decision tree plotting
 
 def compare_data (original_data, fake_data, size_of_fake, mode="save"):
     dataAtts = DataAtts(original_data)
@@ -35,5 +38,61 @@ def compare_data (original_data, fake_data, size_of_fake, mode="save"):
         elif mode=="show":
             plt.show()
         plt.clf()
+        
+def create_comparing_table(original_data_name, fake_data_name):
+    
+    dataAtts = DataAtts(original_data_name)    
+    data = pd.read_csv(original_data_name)
+    fake_data = pd.read_csv(fake_data_name)
+    fake_data.loc[getattr(fake_data, dataAtts.class_name) >= 0.5, dataAtts.class_name] = 1
+    fake_data.loc[getattr(fake_data, dataAtts.class_name) < 0.5, dataAtts.class_name] = 0
+
+    # Creates the training set
+    training_data = [["original", data.head(int(data.shape[0]*0.7))]]
+    fake_name = "fake" + str(fake_data_name).split("/")[2][0]
+    training_data.append([fake_name, fake_data.head(int(fake_data.shape[0]*0.7))])
+    
+    test = data.tail(int(data.shape[0]*0.3))
+    
+    print("| Database \t| Proportion \t| Test Error \t|")
+    print("| ---------\t| ---------: \t| :--------- \t|")
+
+    for episode in training_data:
+            name = episode[0]
+            train = episode[1]
+            try:
+                positive=str(round(train[dataAtts.class_name].value_counts()[0]/len(train) * 100,2))
+            except:
+                positive="0"
+            try:
+                negative=str(round(train[dataAtts.class_name].value_counts()[1]/len(train) * 100,2))
+            except:
+                negative="0"
+
+
+            trainX = train.drop(dataAtts.class_name, 1)
+            testX = test.drop(dataAtts.class_name, 1)
+            y_train = train[dataAtts.class_name]
+            y_test = test[dataAtts.class_name]
+            #trainX = pd.get_dummies(trainX)
+
+            clf1 = DT(max_depth = 3, min_samples_leaf = 1)
+            clf1 = clf1.fit(trainX,y_train)
+            export_graphviz(clf1, out_file="models/tree.dot", feature_names=trainX.columns, class_names=["0","1"], filled=True, rounded=True)
+            g = pydotplus.graph_from_dot_file(path="models/tree.dot")
+
+            pred = clf1.predict_proba(testX)
+            if pred.shape[1] > 1:
+                pred = np.argmax(pred, axis=1)
+            else:
+                pred = pred.reshape((pred.shape[0]))
+                if negative=="0":
+                    pred = pred-1
+
+            mse = round(((pred - y_test.values)**2).mean(axis=0), 4)
+
+            string="| " + name + " \t| " + positive + "/" + negative + " \t| " + str(mse) + " \t|"
+            print(string)
+
     
     
